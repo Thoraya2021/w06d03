f86042b7bd09d9abf432676f4f127ad1197e92c0
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-18 02:31:25 +0300

    controllers

---
+++ backend/routers/controllers/todocontrollers.js
@@ -0,0 +1,85 @@
+let todos=[{id:1,
+    name:"sleep",
+    isDelete:false,
+    isCompleted:false
+    },
+    {id:2,
+        name:"code",
+        isDelete:false,
+        isCompleted:false
+        },
+        {id:3,
+            name:"eat",
+            isDelete:false,
+            isCompleted:false
+            }];
+
+
+const gettodo=(req,res) => {
+    
+    const{id}=req.params;
+    try {
+    const found= todos.find((item)=>{
+    return item.id ==id;
+    
+    });
+    
+    res.stutus(200).json(found);
+    }catch(error){
+    
+    
+        res.stutus(400).json(error)
+    
+    }};
+   const addtodo = (req,res)=>{
+   const newtask={
+    id:todos.length+1,
+       name :req.body.task,
+       isDelete:false,
+       isCompleted:false,
+   }
+   todos.push(newtask);
+   try{
+   res.status(200).json(todos);
+   }
+   catch (error){
+   res.status(400).json(error);
+   
+   }
+   }
+   const updatetodo=(req,res)=>
+   {
+   const {id}=req.params;
+   todos.forEach ((ele)=>{
+   if(ele.id==id){
+   
+   ele.isCompleted=true;
+   
+   }
+   
+   res.status(200).json(todos)
+   });
+
+
+   const deletetodo =(req,res)=>
+   {
+   const {id}=req.params;
+   todos.forEach((ele)=>{
+   if(ele.id==id){
+   
+   ele.isDelete=true;
+   
+   });
+
+   res.status(200).json(todos);
+   
+   }
+   }
+
+   module.exports = {
+
+   gettodo,
+   addtodo,
+   updatetodo,
+   deletetodo,
+   }
