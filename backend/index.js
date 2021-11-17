339ce9cb06ddebc904d8d889549329647551935a
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-11-18 01:17:03 +0300

    todo

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const morgan = require("morgan");
 const cors = require("cors");
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 
 app.use(cors());
 app.use(express.json());
@@ -60,9 +60,6 @@ let todos=[{id:1,
         
         res.stutus(200).json(found);
         }catch(error){
-        
-        
-        
             res.stutus(400).json(error)
         
         }
@@ -71,9 +68,58 @@ let todos=[{id:1,
     
     
 
+app.post("/todos",(req,res)=>
+
+{
+
+const newtask={
+
+    id:todos.length+1,
+    name :req.body.task,
+    isDelete:false,
+    isCompleted:false,
+};
+todos.push(newtask);
+
+try{
+res.status(200).json(todos);
+}
+catch (error){
+res.status(400).json(error);
+
+}
+});
+
+app.put("/update/:id",(req,res)=>
+{
+const {id}=req.params;
+todos.forEach((ele)=>{
+if(ele.id==id){
+
+ele.isCompleted=true;
+
+}
+
+res.status(200).json(todos);
+})
+})
+app.delete("/delete/:id",(req,res)=>
+{
+const {id}=req.params;
+todos.forEach((ele)=>{
+if(ele.id==id){
+
+ele.isDelete=true;
+
+}
+res.status(200).json(todos);
+
+})
+})
+
         const PORT = 4000
 
         app.listen(PORT, ()=>{
-            console.log(`server running at port ${PORT}`);
+            console.log(`server running at port ${PORT}`)
         })
-            
+    
